33e0b94514480c70de31e6a85046e1f5d09063d6
kmd-shafi <shaikshafi5259@gmail.com>
2023-12-25 20:10:14 +0530

    tic tac toe

---
+++ script.js
@@ -0,0 +1,98 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; //playerX,playerO
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetGame = () =>{
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide"); 
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        // console.log("box was clicked");
+       if(turnO) {
+        //playerO
+        box.innerText = "O";
+        turnO = false;
+       } else{
+        //playerX
+        box.innerText = "X";
+        turnO = true;
+       }
+       box.disabled = true; 
+       
+       checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+
+const showWinner = (Winner) =>{
+    msg.innerText = `Congratulations, Winner is ${Winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;    // down 
+        let pos1Va2 = boxes[pattern[1]].innerText;
+        let pos1Va3 = boxes[pattern[2]].innerText;
+
+        if(pos1Val !="" && pos1Va2 !="" && pos1Va3 !=""){
+            if(pos1Val === pos1Va2 && pos1Va2 === pos1Va3){
+                // console.log("winner", pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+           
+    }
+};
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(pattern[0], pattern[1], pattern[2]);
+//         console.log(
+//             boxes[pattern[0]].innerText, 
+//             boxes[pattern[1]].innerText, 
+//             boxes[pattern[2]].innerText
+//             );
